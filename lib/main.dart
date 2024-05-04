34bbdc613d2ed80014620c01ba81ae02afa00d07
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-05-04 17:30:43 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:classical_education/screens/course_details_screen.dart';
+import 'package:classical_education/screens/courses_screen.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CourseDetailsScreen(),
+    );
+  }
+}
